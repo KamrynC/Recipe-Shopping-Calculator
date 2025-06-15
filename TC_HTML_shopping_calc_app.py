fb1c111fd0b2de675ef7926a01b4448e2eb47c3a
KamrynC <kamryncullen@gmail.com>
2025-06-15 03:06:02 -0400

    Sorting file names

--- TC_HTML_shopping_calc_app.py
+++ TC_HTML_shopping_calc_app.py
@@ -56,7 +56,7 @@ st.markdown(
 
 # FILE SELECTION
 UPLOAD_FOLDER = "./html_files"
-available_files = [f for f in os.listdir(UPLOAD_FOLDER) if f.endswith(".html")]
+available_files = sorted([f for f in os.listdir(UPLOAD_FOLDER) if f.endswith(".html")])
 st.markdown("<br>", unsafe_allow_html=True)
 selected_files = st.multiselect("Select all applicable recipe day files to include in the list:", available_files)
 st.markdown("<p style='font-size: 0.9rem; font-weight: bold;'>(Please note that these recipe files are for full-day camps)</p>", unsafe_allow_html=True)
